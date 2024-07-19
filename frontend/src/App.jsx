4f12d114b0f1827a83e9ae8645e646b932ae8da0
Ale0602 <126637451+Ale0602@users.noreply.github.com>
2024-07-18 23:23:04 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Dashboard from './views/Dashboard'
 import Login from './views/Login'
+import Listado from './views/Listado/Listado.jsx'
+<Route path="/listado" element={<Listado />} />
 import { Route, Routes } from "react-router-dom"
 
 function App() {
@@ -10,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
-          
+        <Route path="/listado" element={<Listado />} />
       </Routes>
     </>
   )
