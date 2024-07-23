54d23e0854e6dbac508af267b2762852df7862c4
unknown <javier17utn@gmail.com>
2024-07-22 21:42:07 -0300

    funcionalidad de login form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,20 +4,24 @@ import Home from "./views/Home/Home";
 import Login from "./views/Login";
 import { Route, Routes } from "react-router-dom";
 import Calendario from "./views/CalendarioProfesional";
-import Hiro from "./views/Hero/Hero";
+import Hero from "./views/Hero/Hero";
+import AuthOutlet from "@auth-kit/react-router/AuthOutlet";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Hiro />} />
+        <Route path="/" element={<Hero />} />
 
         <Route path="/login" element={<Login />} />
+        <Route element={<AuthOutlet fallbackPath="/login" />}>
         <Route path="/dashboard" element={<Dashboard />}>
           <Route path="/dashboard/home" element={<Home />} />
           <Route path="/dashboard/calendario" element={<Calendario />} />
           <Route path="/dashboard/paciente" element={"paciente"} />
         </Route>
+        </Route>
+        
       </Routes>
     </>
   );
