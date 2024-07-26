99eb9129e53b4fb7f064c4c3363beea5f3705250
leoyasu <leandroyasutake@gmail.com>
2024-07-25 22:19:37 -0300

    Revert "crud diagnosis funciona"
    This reverts commit e0809e68144139b9094878026f90e1e0d29c93cc.

--- backend/src/config/database/associations.js
+++ backend/src/config/database/associations.js
@@ -3,9 +3,6 @@ import Doctor from "../../modules/professional/proffesional.model.js";
 import User from "../../modules/users/userModel.js";
 import Appointment from "../../modules/appointment/appointment.model.js";
 
-
-import Diagnosis from "../../modules/diagnosis/diagnosisModel.js";
-
 export const initModel = () => {
   User.hasOne(Patient, { foreignKey: "userId" });
   Patient.belongsTo(User, { foreignKey: "userId" });
@@ -16,10 +13,4 @@ export const initModel = () => {
   Appointment.belongsTo(Doctor, { foreignKey: "professionalId" });
   Patient.hasMany(Appointment, { foreignKey: "patientId" });
   Appointment.belongsTo(Patient, { foreignKey: "patientId" });
-  
-  Doctor.hasMany(Diagnosis, { foreignKey: 'doctorId' });
-  Diagnosis.belongsTo(Doctor, { foreignKey: 'doctorId' });
-  
-  Patient.hasMany(Diagnosis, { foreignKey: 'patientId' });
-  Diagnosis.belongsTo(Patient, { foreignKey: 'patientId' });
 };
