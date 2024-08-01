4316a4ad5d2e67144d8eb9a5bb100967a39a6df6
Frere-Luz <algo_de_luz@hotmail.com>
2024-08-01 09:29:07 -0300

    PatientsHome redone, se desconfiguraron las cards

--- frontend/src/views/PatientsHome/PatientsHome.jsx
+++ frontend/src/views/PatientsHome/PatientsHome.jsx
@@ -10,19 +10,20 @@ import { Link } from 'react-router-dom';
 export const PatientsHome = () => {
     return (
         <div>
-            <Container maxWidth={'100vw'} height={'100vh'} overflow={'hidden'} className="bg-cyan-400 flex-wrap patientshome-contenedor">
+            <Container maxWidth={'100vw'} height={'100vh'}
+             className="bg-cyan-400 flex flex-wrap patientshome-contenedor">
                 <NavPatientsHome />
-                <img src={Logo} alt="logo_app_CHealth" width={'70px'} className='z-10 relative top-2 left-10 patientshome-logo' />
-                <Flex direction={"row"} width={'100vw'} gapX={'9'} justify={'center'} align={'center'} pt={'3'} className='relative flex-wrap patientshome'>
-                    <Text className='text-white frase font-medium px-3 text-wrap text-left float-start' width="50%">
+                <img src={Logo} alt="logo_app_CHealth" width={'70px'} className='z-10 absolute top-4 left-10 patientshome-logo' />
+                <Flex direction={"row"} width={'100vw'} justify={'center'} align={'center'} className='flex-wrap patientshome'>
+                    <Text className='text-white frase font-medium text-wrap text-left float-start'>
                         Encuentra tu especialista y pide cita.
                     </Text>
                     <div>
-                        <img src={ImagenDocs} width={'200px'} alt="vectores_Docs" className='float-end patientshome_img' align="center" />
+                        <img src={ImagenDocs} width={'200px'} alt="vectores_Docs" className='float-end top-2 patientshome_img' align="center" />
                     </div>
                 </Flex>
                 <Flex justify={'center'} align={'center'} gapX={'4'} className='inputs'>
-                    <div className="flex border-0 outline-none align-middle text-center p-1">
+                    <div className="flex border-0 outline-none align-middle text-center">
                         <select className="outline-none rounded-lg py-2 px-10 text-center shadow-md shadow-slate-800"
                             id="select_especialidades">
                             <option value="" disabled selected>Especialidad</option>
@@ -39,21 +40,19 @@ export const PatientsHome = () => {
                     </Link>
                    
                 </Flex>
-                <Flex justify={"center"} align={"start"} className='cards'>
-                    <Flex direction={'column'} align={'start'} justify={'center'} className="grp-cards bg-opacity-20 text-left bg-cyan-800">
+                <Flex justify={"center"} align={"center"} direction={'row'} gap={'3'} className='cardsPH'>
+                    <Flex direction={'column'} align={'center'} justify={'center'} className="grp-cards bg-opacity-20 text-left bg-cyan-800">
                         <h2 className="text-slate-800 font-medium">Encuentra tu especialista </h2>
                         <p className="text-slate-800 grpcards-p">Las opiniones reales de miles de pacientes
                             te ayudarán a tomar siempre la mejor decisión</p>
                     </Flex>
-                    <Flex direction={'column'} align={'start'} justify={'center'} className="grp-cards flex-wrap bg-opacity-20 text-left bg-cyan-800">
+                    <Flex direction={'column'} align={'center'} justify={'center'} className="grp-cards flex-wrap bg-opacity-20 text-left bg-cyan-800">
                         <h2 className="text-slate-800 font-medium">Pide cita de forma fácil </h2>
-                        <p className="text-slate-800 grpcards-p">Elige la hora que prefieras y pide cita sin necesidad de llamar. 
-                            Es fácil, cómodo y muy rápido.</p>
+                        <p className="text-slate-800 grpcards-p">Elige la hora que prefieras y pide cita sin necesidad de llamar. Es fácil, cómodo y muy rápido.</p>
                     </Flex>
-                    <Flex direction={'column'} align={'start'} justify={'center'} className="grp-cards bg-opacity-20 text-left bg-cyan-800">
+                    <Flex direction={'column'} align={'center'} justify={'center'} className="grp-cards bg-opacity-20 text-left bg-cyan-800">
                         <h2 className="text-slate-800 font-medium">Recordatorios por Mail </h2>
-                        <p className="text-slate-800 grpcards-p">Te confirmamos la cita al instante y te enviamos un recordatorio 
-                            a tu mail antes de la cita.</p>
+                        <p className="text-slate-800 grpcards-p">Te confirmamos la cita al instante y te enviamos un recordatorio a tu mail antes de la cita.</p>
                     </Flex>
                 </Flex>
             </Container>
